fa6f352e99057beaf93ad5e99ef6abad8b5279a2
squad31fcamara <81252681+squad31fcamara@users.noreply.github.com>
2021-03-24 09:50:57 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express (); 
-const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
     //Conexão com banco de dados 
-    const Sequelize = require('sequelize')
     const sequelize = new Sequelize('fcamara','root','12345', {
     host: "localhost",
     dialect: 'mysql'
     })
-
+    //Rotas 
+    app.get('/', function(req, res){
+        res.sendFile(__dirname +"/index.html")
+    })
+    app.get('/cadastroaluno', function(req, res){
+        res.sendFile(__dirname +"/cadastroaluno.html")
+    })
+    app.get('/cadastrodoador', function(req, res){
+        res.sendFile(__dirname +"/cadastrodoador.html")
+    })
     app.listen(8081, function(){
     console.log("Está rodando em http://localhost:8081");});
