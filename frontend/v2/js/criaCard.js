27383b46d993fbb4044b6a48269b77785e906546
nathaliadv <nathaliadantasv@gmail.com>
2021-03-30 22:47:56 -0300

    alteraçoes de html

--- frontend/v2/js/criaCard.js
+++ frontend/v2/js/criaCard.js
@@ -11,7 +11,7 @@ export default function criaCard(imagem, nome, nivel, ano, municipio, uf, sonho,
         <h5 class="card-title">${nome} - ${nivel}, ${ano}</h5>
         <p class="card-text">${municipio}, ${uf}</p>
         <p class="card-text font-italic">${sonho}</p>
-        <button class="btn btn-primary button-aluno" id=${id} type="submit"><a href="./aluno.html">Quero doar!<a></button>
+        <button class="btn btn-secondary button-aluno" id=${id} type="submit"><a href="./aluno.html">Quero doar!<a></button>
     </div>
     `
 
