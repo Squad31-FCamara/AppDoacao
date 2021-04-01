d638a289d7be95f66d2020ac9b8c1a73969d8411
nathaliadv <nathaliadantasv@gmail.com>
2021-04-01 15:51:14 -0300

    mudança de estado e retorno de mensagem para o usuario

--- frontend/v2/js/mudaStatusKit.js
+++ frontend/v2/js/mudaStatusKit.js
@@ -1,7 +1,7 @@
 function mudaStatusKit() {
 
     const botaoSubmit = document.querySelector('#botao-agendar')
-    botaoSubmit.addEventListener('click', event => {
+    botaoSubmit.addEventListener('click', (event) => {
         event.preventDefault()
 
         function getValues() {
@@ -18,9 +18,21 @@ function mudaStatusKit() {
 
         ids.map(id => {
             console.log(id)
-            axios.get(`http://localhost:3333/kit/${id}/status`)
-            .then(response => console.log(response.data))
+            axios.patch(`http://localhost:3333/kit/${id}/status`)
+            .then(response => criarAlerta(response.data))
             .catch(error => console.log(error))
+
+            const criarAlerta = () => {
+                const alerta = document.querySelector('#mensagem')
+                alerta.setAttribute('class', 'alert alert-success')
+                alerta.setAttribute('role', 'alert')
+                
+                alerta.innerHTML = `
+                <strong>Doação agendada com sucesso!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button> `
+            }
         })
 
     })
