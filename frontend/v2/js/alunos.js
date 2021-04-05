e7d92dca883f7c1ce2b7589eef13c9de482f4999
nathaliadv <nathaliadantasv@gmail.com>
2021-04-05 12:50:24 -0300

    filtro dos cards dos alunos

--- frontend/v2/js/alunos.js
+++ frontend/v2/js/alunos.js
@@ -1,60 +1,65 @@
-import criaCard from './criaCard.js'
-import criaElemento from './escola-sem-alunos.js'
+import criaCard from "./criaCard.js";
+import criaElemento from "./escola-sem-alunos.js";
 
 async function getDonatarios(id_escola) {
-        
-    const resposta = await axios.get(`http://localhost:3333/donatarios/escola/${id_escola}`)
-    const resultado = await resposta.data
-    return resultado
+  const resposta = await axios.get(
+    `http://localhost:3333/donatarios/escola/${id_escola}`
+  );
+  const resultado = await resposta.data;
+  return resultado;
 }
 
 async function getAlunosPorEscola() {
-    
-    //const id = window.location.href.split("?id=")[1]
-    const id_escola = sessionStorage.getItem("id_escola")
-    
-    const resultado = await getDonatarios(id_escola)
-    
-    if(resultado.length == 0){
-        criaElemento()
-    }
+  const id_escola = sessionStorage.getItem("id_escola");
 
-    resultado.map(aluno => criaCard(aluno.avatar, aluno.nome_escola, aluno.segmento_ensino, aluno.serie_ensino, aluno.cidade, aluno.uf, aluno.sonho_profissao, aluno.id_donatario, aluno.tipo)) 
+  const resultado = await getDonatarios(id_escola);
 
-    const buttonsAluno = document.querySelectorAll(".button-aluno")
+  if (resultado.length == 0) {
+    criaElemento();
+  }
 
-    buttonsAluno.forEach( button => button.addEventListener('click', () => {
-       
-        sessionStorage.setItem("id_aluno", button.id)
-        })
+  resultado.map((aluno) =>
+    criaCard(
+      aluno.avatar,
+      aluno.nome_escola,
+      aluno.segmento_ensino,
+      aluno.serie_ensino,
+      aluno.cidade,
+      aluno.uf,
+      aluno.sonho_profissao,
+      aluno.id_donatario,
+      aluno.tipo
     )
+  );
 
-        /* 
-    if(window.location.href !== "http://127.0.0.1:5500/frontend/v2/aluno.html"){
-        sessionStorage.clear()
-    } */
-    
+  const buttonsAluno = document.querySelectorAll(".button-aluno");
 
+  buttonsAluno.forEach((button) =>
+    button.addEventListener("click", () => {
+      sessionStorage.setItem("id_aluno", button.id);
+    })
+  );
 }
 
-getAlunosPorEscola()
+getAlunosPorEscola();
 
-async function filtraCards(valor) {
-    
-    const id_escola = sessionStorage.getItem("id_escola")
+const buttons = document.querySelectorAll(".tipo_doacao");
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    filtra(button.value);
+  })
+);
 
-    const resultado = await getDonatarios(id_escola)
+function filtra(valor) {
+  const cards = document.querySelectorAll(".default");
 
-    const tipoDoacao = resultado.filter((doacao) => {return doacao.tipo === `${valor}`})
-
-    tipoDoacao.map(aluno => criaCard(aluno.avatar, aluno.nome_escola, aluno.segmento_ensino, aluno.serie_ensino, aluno.cidade, aluno.uf, aluno.sonho_profissao, aluno.id_donatario, aluno.tipo)) 
-
-    const buttonsAluno = document.querySelectorAll(".button-aluno")
-
-    buttonsAluno.forEach( button => button.addEventListener('click', () => {
-       
-        sessionStorage.setItem("id_aluno", button.id)
-        })
-    )
-
+  for (let i = 0; i < cards.length; i++) {
+    if (valor == cards[i].classList[0]) {
+      cards[i].style = "display: inline-block";
+    } else if (valor == "") {
+      cards[i].style = "display: inline-block";
+    } else {
+      cards[i].style = "display: none";
+    }
+  }
 }
--- frontend/v2/js/criaCard.js
+++ frontend/v2/js/criaCard.js
@@ -1,11 +1,22 @@
-export default function criaCard(imagem, nome_escola, nivel, ano, municipio, uf, sonho, id, tipo) {
-    const conjCard = document.querySelector('.card-columns')
-    
-    const card = document.createElement('div')
-    card.setAttribute('class', `card text-center my-3 ${tipo}`)
-    card.setAttribute('id', `${id}`)
+export default function criaCard(
+  imagem,
+  nome_escola,
+  nivel,
+  ano,
+  municipio,
+  uf,
+  sonho,
+  id,
+  tipo
+) {
+  const conjCard = document.querySelector(".card-columns");
 
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.setAttribute("class", `${tipo} card text-center my-3 default`);
+  card.setAttribute("id", `${id}`);
+  card.setAttribute("value", `${tipo}`);
+
+  card.innerHTML = `
     <img class="mx-auto p-3" src="../imgs/${imagem}" width="130" height="130" alt="Avatar do aluno">
     <div class="card-body">
         <h5 class="card-title">${nome_escola} - ${nivel}, ${ano}</h5>
@@ -13,7 +24,7 @@ export default function criaCard(imagem, nome_escola, nivel, ano, municipio, uf,
         <p class="card-text font-italic">${sonho}</p>
         <button style="color:white; background-color:#37B4AA; border:none; " class="btn btn-secondary button-aluno" id=${id} type="submit"><a style="color:white;" href="./aluno.html">Quero doar!<a></button>
     </div>
-    `
+    `;
 
-    conjCard.append(card) 
-} 
+  conjCard.append(card);
+}
